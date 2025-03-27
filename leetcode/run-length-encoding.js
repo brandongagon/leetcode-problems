624d63331ab93484a393145bb00c61f7648185eb
brandongagon <brandongagon@gmail.com>
2025-03-26 22:45:24 -0700

    run length encoding

---
+++ leetcode/run-length-encoding.js
@@ -0,0 +1,20 @@
+
+function encode(str) {
+  if (!str) return '';
+  
+  let output = '';
+  let count = 1;
+
+  for (let i=0;i<str.length;i++) {
+    if (i + 1 < str.length && str[i] === str[i + 1]) {
+      count++;
+    } else {
+      output += count + str[i];
+      count = 1;
+    }
+  }
+
+  return output;
+}
+
+console.log(encode('aabbbbbc'));
