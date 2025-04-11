6a02dc554db25c473aff35b3bcf38615619a79c5
brandongagon <brandongagon@gmail.com>
2025-04-10 17:17:20 -0700

    code for maxProfit

---
+++ leetcode/maxProfit.js
@@ -0,0 +1,17 @@
+/*
+ Best time to buy and sell a stock
+*/
+
+function maxProfit(prices) {
+  let profit = 0;
+  let min = prices[0];
+
+  for (let i=0;i<prices.length;i++) {
+    if (min > prices[i]) {
+      min = prices[i];
+    } else if (prices[i] - min > profit) {
+      profit = prices[i] - min;
+    }
+  }
+  return profit;
+}
