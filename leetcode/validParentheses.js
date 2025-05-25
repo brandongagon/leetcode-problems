affeb7418ba9dceae2cc9387deb64d0887cedf2c
brandongagon <brandongagon@gmail.com>
2025-05-25 16:24:56 -0700

    is valid parentheses

---
+++ leetcode/validParentheses.js
@@ -0,0 +1,25 @@
+function isValid(s) {
+  let mappings = {
+    ")":"(",
+    "}":"{",
+    "]":"["
+  };
+  let stack = [];
+
+  for (let c of s) {
+    if (mappings[c]) {
+      let topChar = stack.length ? stack.pop() : "#";
+      if (topChar !== mappings[c]) {
+        return false;
+      }
+    } else {
+      stack.push(c);
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isValid("{}"));
+console.log(isValid("{})"));
+console.log(isValid("{}#"));
