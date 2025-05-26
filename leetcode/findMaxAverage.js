a287177254f22ac6776877002488638a9d157e1f
brandongagon <brandongagon@gmail.com>
2025-05-26 14:31:20 -0700

    find max average solution

---
+++ leetcode/findMaxAverage.js
@@ -0,0 +1,14 @@
+
+function findMaxAverage(nums, k) {
+  let currentSum = 0;
+  for (let i=0;i<k;i++) {
+    currentSum += nums[i]
+  }
+  
+  let maxSum = currentSum;
+  for (let i=k;i<nums.length;i++) {
+    currentSum = nums[i] - nums[i - k];
+    maxSum = Math.max(currentSum, maxSum);
+  }
+  return maxSum / k;
+}
